b64f1b2753aeb0de34d5641a0b339c5032477d0e
TreXBuzz <ishangoel901@gmail.com>
2022-01-08 17:47:03 +0530

    SPA 1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,36 +1,54 @@
-import Menu from './MenuComponent';
-import Home from './HomeComponent';
-import {DISHES} from '../shared/dishes' ;
-import { Component } from 'react';
+import Menu from "./MenuComponent";
+import Home from "./HomeComponent";
+import Contact from "./ContactComponent";
+import { DISHES } from "../shared/dishes";
+import { COMMENTS } from "../shared/comments";
+import { LEADERS } from "../shared/leaders";
+import { PROMOTIONS } from "../shared/promotions";
+import { Component } from "react";
 import DetailedDish from "./DetailedDishComponent";
-import Header from './HeaderComponent' ;
-import Footer from './FooterComponent';
-import { Route, Routes, Navigate} from 'react-router-dom';
+import Header from "./HeaderComponent";
+import Footer from "./FooterComponent";
+import { Route, Routes, Navigate } from "react-router-dom";
 class Main extends Component {
-
-  constructor(props){
+  constructor(props) {
     super(props);
 
     this.state = {
-      dishes : DISHES,
+      dishes: DISHES,
+      leaders: LEADERS,
+      promotions: PROMOTIONS,
+      comments: COMMENTS,
     };
   }
 
-  render(){
-
-  
-  return (
-    <div>
-    <Header/>
-      <Routes>
-        <Route path="/" element={<Navigate replace to="/home" />} />
-        <Route path="/home" element={<Home/>}/>
-        <Route path="/menu" element={<Menu dishes={this.state.dishes}/>} />
-      </Routes>
-      <Footer/>
-    </div>
-  );
-}
+  render() {
+    return (
+      <div>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Navigate replace to="/home" />} />
+          <Route
+            path="/home"
+            element={
+              <Home
+                dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+                leader={
+                  this.state.leaders.filter((leader) => leader.featured)[0]
+                }
+                promotion={
+                  this.state.promotions.filter((promo) => promo.featured)[0]
+                }
+              />
+            }
+          />
+          <Route path="/menu" element={<Menu dishes={this.state.dishes} />} />
+          <Route path="/contactus" element={<Contact />} />
+        </Routes>
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default Main;
