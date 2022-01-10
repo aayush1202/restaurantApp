91de623cf2c3189ab83da5f600adbdbf6ec84608
TreXBuzz <ishangoel901@gmail.com>
2022-01-10 18:11:25 +0530

    React Forms wihtout dish detail working

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -6,6 +6,14 @@ import {
   NavbarToggler,
   Collapse,
   NavItem,
+  Modal,
+  ModalBody,
+  Button,
+  ModalHeader,
+  Form,
+  FormGroup,
+  Label,
+  Input
 } from "reactstrap";
 import { NavLink } from "react-router-dom";
 class Header extends Component {
@@ -13,14 +21,21 @@ class Header extends Component {
     super(props);
     this.state = {
       isNavOpen: false,
+      isModalOpen : false
     };
 
     this.toggleNav = this.toggleNav.bind(this);
+    this.toggleModal = this.toggleModal.bind(this);
   }
 
   toggleNav() {
     this.setState({
       isNavOpen: !this.state.isNavOpen,
+    });
+  }
+  toggleModal(){
+    this.setState({
+      isModalOpen: !this.state.isModalOpen,
     });
   }
 
@@ -65,6 +80,13 @@ class Header extends Component {
                     </NavLink>
                   </NavItem>
                 </Nav>
+                <Nav className="ml-auto" navbar>
+                  <NavItem>
+                    <Button outline onClick={this.toggleModal}>
+                      <span className="fa fa-sign-in fa-lg"></span>Login
+                    </Button>
+                  </NavItem>
+                </Nav>
               </Collapse>
             </div>
           </div>
@@ -82,6 +104,28 @@ class Header extends Component {
             </div>
           </div>
         </div>
+        <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+          <ModalHeader toggle={this.toggleModal}>Login</ModalHeader>
+          <ModalBody>
+            <Form onSubmit={this.handleLogin}>
+              <FormGroup>
+                <Label htmlFor="username" >Username</Label>
+                <Input type="text" id="username" name="username" />
+              </FormGroup>
+              <FormGroup>
+                <Label htmlFor="password" >Password</Label>
+                <Input type="password" id="password" name="password" />
+              </FormGroup>
+              <FormGroup check>
+                <Label check>
+                  <Input type="checkbox" name="remember" /> 
+                  Remember me
+                </Label>
+              </FormGroup>
+              <Button type="submit" value="submit" className="primary">Login</Button>
+            </Form>
+          </ModalBody>
+        </Modal>
       </React.Fragment>
     );
   }
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,5 +1,7 @@
+import React from 'react';
 import Menu from "./MenuComponent";
 import Home from "./HomeComponent";
+import About from './AboutComponent';
 import Contact from "./ContactComponent";
 import { DISHES } from "../shared/dishes";
 import { COMMENTS } from "../shared/comments";
@@ -9,7 +11,8 @@ import { Component } from "react";
 import DetailedDish from "./DetailedDishComponent";
 import Header from "./HeaderComponent";
 import Footer from "./FooterComponent";
-import { Route, Routes, Navigate } from "react-router-dom";
+import { Route, Routes, Navigate, useParams} from "react-router-dom";
+
 class Main extends Component {
   constructor(props) {
     super(props);
@@ -23,11 +26,14 @@ class Main extends Component {
   }
 
   render() {
-    return (
+    
+      return (
+
+        
       <div>
         <Header />
         <Routes>
-          <Route path="/" element={<Navigate replace to="/home" />} />
+          <Route path="*" element={<Navigate replace to="/home" />} />
           <Route
             path="/home"
             element={
@@ -43,7 +49,10 @@ class Main extends Component {
             }
           />
           <Route path="/menu" element={<Menu dishes={this.state.dishes} />} />
+          {/* <Route path="/menu/:dishId"  element={ <DetailedDish dish={this.state.dishes.filter((dish) => dish.id === parseInt({dishId},10))[0]} 
+        comments={this.state.comments.filter((comment) => comment.dishId === parseInt({dishId},10))[0]} />} /> */}
           <Route path="/contactus" element={<Contact />} />
+          <Route path="/aboutus" element={<About  leader = {this.state.leaders} />} />
         </Routes>
         <Footer />
       </div>
